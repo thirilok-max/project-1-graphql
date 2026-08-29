3dd2741da1786e06b77f4ee1b98a3edbb442d317
thirilok-max <thirilok@webizze.com>
2026-08-29 17:50:22 +0530

    file is added

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+// import schema from "./schemaUsers/schema.js"
+import { createHandler} from "graphql-http/lib/use/express";
+import {schema,rootSchema} from "./schemaUsers/schema.js"
+import db from "./models/index.js";
+const app=express();
+const PORT=process.env.PORT;
+app.use(express.json());
+
+app.use("/graphql",createHandler({
+    schema:schema,
+    rootValue:rootSchema, 
+}))
+app.listen(PORT,()=>{
+    console.log(`server is running @ http:localhost${PORT}`)
+})
+const server=async()=>{
+    await db.sequelize.authenticate({alter:true});
+    console.log("db is connected");
+    // await db.sequelize.sync({force:true});
+    // console.log("db is refreshed");
+}
+server();
